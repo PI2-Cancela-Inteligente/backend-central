41286084b846c6130ee2210505cec08bf7db7320
Sayuck <nobrega.m.roberto@gmail.com>
2023-01-19 19:47:51 -0300

    Adiciona Cors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 from src.routers import carro, usuario, estaciona, motorista
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(carro.router, tags=["Carro"])
 app.include_router(usuario.router, tags=["Usuario"])
